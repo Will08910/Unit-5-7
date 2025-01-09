3f5c3de1916612c9fc37d8073a5b65bc2ba0bd26
Will08910 <550564@escg.ac.uk>
2025-01-09 16:00:31 +0000

    music stuff

--- Assets/VolumeControl.cs
+++ Assets/VolumeControl.cs
@@ -1,29 +1,114 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
-public class VolumeControl : MonoBehaviour
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
+
+public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
+
+    public AudioClip[] clips;
+    private AudioSource audioSource;
+
     public Slider volumeSlider;
-    public AudioSource audioSource;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    void Awake()
     {
-        if (volumeSlider != null && audioSource != null)
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+        
+
+        
+        InitializeVolumeSlider();
+
+        PlayAudioForScene(SceneManager.GetActiveScene().name);
+    }
+
+    void OnEnable()
+    {
+        
+        InitializeVolumeSlider();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void InitializeVolumeSlider()
+    {
+        if (volumeSlider == null)
+        {
+            volumeSlider = GameObject.FindGameObjectWithTag("VolumeSlider")?.GetComponent<Slider>();
+        }
+
+        if (volumeSlider != null)
         {
             volumeSlider.value = audioSource.volume;
+            volumeSlider.onValueChanged.AddListener(OnVolumeChanged); 
         }
-    }
-
-    public void SetVolume(float volume)
-    {
-        if (audioSource != null)
+        else
         {
-            audioSource.volume = volume;
+            Debug.LogWarning("Volume Slider is missing! Please assign a Slider in the Inspector.");
+        }
+
+    }
+
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        StopClip();
+        PlayAudioForScene(scene.name);
+
+        InitializeVolumeSlider();
+    }
+
+    private void PlayAudioForScene(string sceneName)
+    {
+        if (sceneName == "FrontEnd")
+        {
+            PlayClip(0);
+        }
+        else if (sceneName == "SampleScene")
+        {
+            PlayClip(1);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayClip(int clipNumber)
     {
-        SetVolume(volumeSlider.value);
+        if (clipNumber < 0 || clipNumber >= clips.Length)
+        {
+            return;
+        }
+
+        
+        audioSource.volume = volumeSlider.value;
+
+        
+        audioSource.PlayOneShot(clips[clipNumber], 1f);  
+    }
+
+
+
+    public void StopClip()
+    {
+        audioSource.Stop();
+    }
+
+    public void OnVolumeChanged(float volume)
+    {
+        audioSource.volume = Mathf.Clamp(volume, 0f, 1f);
     }
 }
---
+++ Assets/kill.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class kill : MonoBehaviour
+{
+    public GameObject settings;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        settings.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
